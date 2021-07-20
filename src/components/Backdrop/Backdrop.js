f7e8bd7679774b1bdd38ea12938ecd8bfa5fc82d
Andrews <allwinandrews@gmail.com>
2021-07-20 11:31:00 +0530

    Displays Modal and Backdrop conditionally

--- src/components/Backdrop/Backdrop.js
+++ src/components/Backdrop/Backdrop.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import './Backdrop.css';
+import "./Backdrop.css";
 
-const backdrop = (props) => (
-    <div className="Backdrop"></div>
-);
+const backdrop = (props) => {
+  const cssClasses = [
+    "Backdrop",
+    props.show ? "BackdropOpen" : "BackdropClosed",
+  ];
+
+  return <div className={cssClasses.join(" ")} onClick={props.closed}></div>;
+};
 
 export default backdrop;
